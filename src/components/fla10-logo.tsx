30d2d3c7f4d1e4e32682af5c57928fa1eeda8ef4
Flamengo Dez <canalfladez@gmail.com>
2025-07-08 20:28:16 +0000

    O link anterior foi errado... Este é o link certo: https://imgur.com/I51

--- src/components/fla10-logo.tsx
+++ src/components/fla10-logo.tsx
@@ -5,7 +5,7 @@ export function Fla10Logo() {
   return (
     <div className="relative h-10 w-10">
       <Image
-        src="https://i.imgur.com/8N45d6C.png"
+        src="https://i.imgur.com/I51Hfzu.png"
         alt="Fla10 News Logo"
         fill
         className="object-contain"
