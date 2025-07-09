f06f4e2784f1d0f5a807c9fd56d1b6815fec7903
Flamengo Dez <canalfladez@gmail.com>
2025-07-09 00:42:03 +0000

    Troque a tabela atual, por esta:
    <div data-widget-type="entityStandings

--- src/components/sofascore-widget.tsx
+++ src/components/sofascore-widget.tsx
@@ -1,21 +1,29 @@
 'use client';
 
 import React from 'react';
+import Script from 'next/script';
 
 export function SofascoreWidget() {
   return (
     <div className="mx-auto w-full max-w-[768px]">
-      <iframe
-        id="sofa-standings-embed-83-72034"
-        src="https://widgets.sofascore.com/embed/tournament/83/season/72034/standings/Brasileiro%20Serie%20A%202025?widgetTitle=Brasileiro%20Serie%20A%202025&showCompetitionLogo=true"
-        style={{ height: '1123px' }}
-        className="w-full border-0"
-        scrolling="no"
-        title="Sofascore standings widget"
-      ></iframe>
+      <div 
+        data-widget-type="entityStandings" 
+        data-entity-type="league" 
+        data-entity-id="113" 
+        data-lang="pt-br" 
+        data-widget-id="366ccc64-c56b-47d0-8d58-bbb9ea042210"
+      ></div>
       <div className="text-xs font-sans text-left mt-1 text-muted-foreground">
-        Standings provided by <a target="_blank" rel="noopener noreferrer" href="https://www.sofascore.com/tournament/football/brazil/brasileirao-serie-a/325#id:72034" className="underline text-primary">Sofascore</a>
+        Desenvolvido por
+        <a href="https://www.365scores.com/pt-br" target="_blank" rel="noopener noreferrer" className="underline text-primary ml-1">
+          365Scores.com
+        </a>
       </div>
+      <Script 
+        id="365scores-widget" 
+        src="https://widgets.365scores.com/main.js" 
+        strategy="lazyOnload" 
+      />
     </div>
   );
 }
