196d748b08fb522ae605cc25b90ac430f5f90b6d
Flamengo Dez <canalfladez@gmail.com>
2025-07-09 17:25:28 +0000

    Porque a foto não está aparecendo

--- src/next.config.ts
+++ src/next.config.ts
@@ -16,6 +16,18 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'imgur.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
