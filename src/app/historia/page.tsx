9392bad9571e188a8976a316af89549d57ce32fc
Flamengo Dez <canalfladez@gmail.com>
2025-07-10 15:06:49 +0000

    MOVER A IMAGEM 200PX PARA BAIXO

--- src/app/historia/page.tsx
+++ src/app/historia/page.tsx
@@ -13,7 +13,7 @@ export default function HistoriaPage() {
       </div>
       <div className="space-y-8">
         <header className="text-center border-b pb-6 mb-4 overflow-hidden">
-          <div className="relative mx-auto h-[18.9rem] w-[18.9rem] mt-[-300px]">
+          <div className="relative mx-auto h-[18.9rem] w-[18.9rem] mt-[-100px]">
             <Image
                 src="https://i.postimg.cc/RF5TbLnK/image-removebg-preview-8.png"
                 alt="Flamengo CRF Logo"
